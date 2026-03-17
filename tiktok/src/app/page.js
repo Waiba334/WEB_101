095ca0382168ba29ab66254ba7585326ef891965
Waiba334 <waibasamuel5@gmail.com>
2026-03-17 12:10:14 +0600

    Practical1

--- tiktok/src/app/page.js
+++ tiktok/src/app/page.js
@@ -1,8 +1,9 @@
+import VideoFeed from '../components/ui/VideoFeed';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">TikTok</h1>
-      <p className="mt-4 text-xl">Welcome to our TikTok project!</p>
-    </main>
+    <div className="py-4">
+      <VideoFeed />
+    </div>
   );
 }
